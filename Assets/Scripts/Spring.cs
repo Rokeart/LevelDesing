b8a2d600cc58e6aca562c45cf9f85c30ae33db7f
Rokeart <tincho.ochoa@gmail.com>
2025-09-16 22:00:42 -0300

    [add] Spring animation

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 public class Spring : MonoBehaviour
 {
     public float bounceForce = 15f;
-    
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
-        { 
-          
+        {
             PlayerController rb = other.gameObject.GetComponent<PlayerController>();
             if (rb != null)
             {
-                                        
+                // impulso hacia arriba
                 rb.velocity.y = bounceForce;
 
+                // dispara la animación de resorte
+                if (animator != null)
+                {
+                    animator.SetTrigger("Bounce");
+                }
             }
         }
     }
